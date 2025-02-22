a26cf8872cc77f23ff5fac4cb8e749e8980e0230
RHN-04 <ransama04@gmail.com>
2025-02-22 19:18:36 +0300

    server v0.2

--- controllers/general.js
+++ controllers/general.js
@@ -14,3 +14,25 @@ exports.education_level = CRUDController({
         delete: async () => null,
     }
 })
+
+exports.lesson = CRUDController({ modelName: "lesson" }) // здесь в findByFields по теме происходит деление на лабы, лекции и практики, не знаю, как перенести аккуратно
+
+exports.professional_module = CRUDController({ modelName: "professional_module" })
+
+exports.semester = CRUDController({ 
+    modelName: "semester",
+    findByFields: [{ fieldName: "subject", orderBy: [['number', 'ASC']] }]
+ })
+
+exports.source = CRUDController({ modelName: "source" })
+
+exports.specialty = CRUDController({ modelName: "specialty" }) // здесь реализован поиск
+
+exports.subject = CRUDController({ modelName: "subject" }) // здесь происходит веселье с транзакциями, потому что вся информация, связанная с часами, хранится в семестре, связанным с предметом, а не в самом предмете
+
+exports.theme = CRUDController({ 
+    modelName: "theme",
+    findByFields: [{ fieldName: "unit", orderBy: [['number', 'ASC']] }]
+})
+
+exports.unit = CRUDController({ modelName: "unit" })
